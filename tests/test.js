3f42597cea6ff5bfb64556a74fc948ba27e802ca
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2025-03-07 13:44:40 -0800

    Update Dependencies (#29)

--- tests/test.js
+++ tests/test.js
@@ -68,7 +68,7 @@ async function getPage(browser, name, log, size) {
         ],
         dumpio: true,
         // headless: false,
-        // slowMo: 150,
+        // slowMo: 50,
     })
     console.log('browser:', browser)
 
@@ -82,6 +82,7 @@ async function getPage(browser, name, log, size) {
     console.log('worker:', worker)
 
     // Popup
+    await new Promise((resolve) => setTimeout(resolve, 500))
     await worker.evaluate('chrome.action.openPopup();')
     let page = await getPage(browser, 'popup.html')
     console.log('page:', page)
